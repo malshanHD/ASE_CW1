ff3083d5ab8511716b20b8252c70160dd4f7d387
Malshan Hansaka <75769208+malshanHD@users.noreply.github.com>
2021-08-03 13:16:04 +0530

    update code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sams & Sams 
+                    Laravel
                 </div>
 
                 <div class="links">
