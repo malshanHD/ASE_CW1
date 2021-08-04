de62eefe0bb399307b8dd73ef7d117c0b49ad270
Malshan Hansaka <75769208+malshanHD@users.noreply.github.com>
2021-08-04 20:26:50 +0530

    developed footer

--- resources/views/signin.blade.php
+++ resources/views/signin.blade.php
@@ -6,6 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign In</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    <style>
+      hr {
+        border-top: 2px solid orange;
+      }
+  
+</style>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="background: linear-gradient(90deg, rgba(233,255,4,1) 0%, rgba(1,21,81,1) 0%, rgba(1,21,81,1) 30%, rgba(1,21,81,1) 61%, rgba(1,21,81,1) 100%);">
@@ -67,6 +75,55 @@
     </div>
 </div>
 
+<div class="container-fluid mt-5 text-light" style="background: rgb(46,46,84);">
+  <div class="container">
+    <div class="row">
+      <div class="col-3 mt-4">
+        <h5>Customer Care</h5>
+        <p>Help Center</p>
+        <p>How to buy</p>
+        <p>Return and refunds</p>
+        <p>Contaact us</p>
+      </div>
+      <div class="col-4 text-center mt-4">
+        <h5>Payment Methods</h5>
+        <i class="fa fa-money fa-4x text-success" aria-hidden="true"></i>  
+        <i class="fa fa-cc-visa fa-4x text-warning" aria-hidden="true"></i> 
+        <i class="fa fa-cc-mastercard fa-4x text-danger" aria-hidden="true"></i>
+        <i class="fa fa-cc-paypal fa-4x text-primary" aria-hidden="true"></i>
+      </div>
+      <div class="col-2 mt-4">
+        <h5>Follow Us</h5>
+        <i class="fa fa-facebook-official fa-2x text-primary" aria-hidden="true"></i>
+        <i class="fa fa-twitter-square fa-2x text-primary" aria-hidden="true"></i>
+        <i class="fa fa-instagram fa-2x text-danger" aria-hidden="true"></i>
+        <i class="fa fa-youtube-square fa-2x text-danger" aria-hidden="true"></i>
+      </div>
+      <div class="col-3 mt-4">
+        <img src="SystemImage/syslogo.png" style="width:100%;" alt="">
+        
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-2">
+        <img src="SystemImage/apple.png" style="width:100%;" alt="">
+      </div>
+      <div class="col-2">
+        <img src="SystemImage/google.png" style="width:100%;" alt="">
+      </div>
+      <div class="col-2">
+        <img src="SystemImage/huawei.png" style="width:100%;" alt="">
+      </div>
+    </div>
+    <div class="row mt-2 justify-content-center">
+      <p>Sams & Sams (Pvt) Ltd </p>
+    </div>
+    <div class="row mt-2 justify-content-center">
+      <p>All Right Reserved 2021</p>
+    </div>
+    
+  </div>
+</div>
 
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
