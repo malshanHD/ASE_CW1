8cf9de669fba4cd243183dcc4076ade64fddca43
Malshan Hansaka <75769208+malshanHD@users.noreply.github.com>
2021-08-05 16:25:34 +0530

    change nav bar

--- resources/views/include/BuyerNavBar.blade.php
+++ resources/views/include/BuyerNavBar.blade.php
@@ -5,22 +5,22 @@
   </button>
   <div class="collapse navbar-collapse justify-content-end text-uppercase font-weight-bold" id="navbarNavDropdown">
     <ul class="navbar-nav">
-      <li class="nav-item ">
+      <li class="nav-item active">
         <a class="nav-link" href="#">Language</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#"><u>Item Buy</u></a>
+        <a class="nav-link" href="#">Item Buy</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="#">Wish List</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="#">Help</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="#">Account</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="#">Logout</a>
       </li>
     </ul>
