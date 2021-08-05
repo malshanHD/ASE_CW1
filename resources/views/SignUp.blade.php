6149f4f5df05f809bead64e6e4814d55dd2d8d44
mithun-1994 <mithunyasangika456@gmail.com>
2021-08-05 11:31:19 +0530

    add footer to Sign up page

--- resources/views/SignUp.blade.php
+++ resources/views/SignUp.blade.php
@@ -10,7 +10,7 @@
 
 </head>
 <body>
- 
+ <!-- nav start-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="background: linear-gradient(90deg, rgba(233,255,4,1) 0%, rgba(1,21,81,1) 0%, rgba(1,21,81,1) 30%, rgba(1,21,81,1) 61%, rgba(1,21,81,1) 100%);">
 <a class="navbar-brand" href="#"><img src="SystemImage/logov1.png" style="width: 200px;" alt=""></a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,7 +39,9 @@
     </ul>
   </div>
 </nav>
+<!-- nav end-->
 
+<!-- Sign-up form start-->
 <div class="container"> 
     <div class="col-md-3"></div>
     <div class="col-md-12" id="form">
@@ -165,69 +167,11 @@
        </div>
     <div class="col-md-3"></div>
 </div>
-  
-<div class="container-fluid mt-5 text-light" style="background: rgb(46,46,84);">
-  <div class="container">
-    <div class="row">
+  <!-- Sign-up form end-->
 
-      <div class="col-12 col-md-3 mt-4">
-        <h5>Customer Care</h5><hr>
-        <p>Help Center</p>
-        <p>How to buy</p>
-        <p>Return and refunds</p>
-        <p>Contaact us</p>
-        <p>About us</p>
-      </div>
-
-      <div class="col-12 col-md-5 text-center mt-4">
-        <h5>Payment Methods</h5>
-        <hr>  
-        <i class="fa fa-cc-visa fa-4x text-warning" aria-hidden="true"></i> 
-        <i class="fa fa-cc-mastercard fa-4x text-danger" aria-hidden="true"></i>
-        <i class="fa fa-cc-paypal fa-4x text-primary" aria-hidden="true"></i>
-        <i class="fa fa-money fa-4x text-success" aria-hidden="true"></i>
-      </div>
-     
-      <div class="col-6 col-md-2 mt-4">
-        <img src="SystemImage/syslogo.png" style="width:100%;" alt="">
-       </div>
-
-       <div class="col-6 col-md-2 mt-4">
-        <img src="SystemImage/qr.png" style="width:100%;" alt="">
-       </div>
-
-    </div>
-    <div class="row justify-content-center">
-      <div class="col-12 col-md-2">
-        <img src="SystemImage/apple.png" style="width:100%;" alt="">
-      </div>
-      <div class="col-12 col-md-2">
-        <img src="SystemImage/google.png" style="width:100%;" alt="">
-      </div>
-      <div class="col-12 col-md-2">
-        <img src="SystemImage/huawei.png" style="width:100%;" alt="">
-      </div>
-    </div>
-    <div class="row mt-2 justify-content-center">
-      <div class="col-12 col-md-12 mt-4 text-center">
-          <h5>Follow Us</h5>
-          <i class="fa fa-facebook-official fa-2x text-primary social" aria-hidden="true"></i>   
-          <i class="fa fa-twitter-square fa-2x text-primary social" aria-hidden="true"></i>
-          <i class="fa fa-instagram fa-2x  social" style="text-shadow: 1px 1px 1px #ccc;" aria-hidden="true"></i>
-          <i class="fa fa-youtube-square fa-2x text-danger social" aria-hidden="true"></i>
-          <i class="fa fa-whatsapp fa-2x text-success social" aria-hidden="true"></i>
-          <i class="fa fa-snapchat-square fa-2x text-warning social" aria-hidden="true"></i>
-          <i class="fa fa-yahoo fa-2x text-info social" aria-hidden="true"></i>
-      </div>
-    </div>
-
-    <div class="row mt-2 justify-content-center">
-      <p>All Right Reserved 2021</p>
-    </div>
-    
-  </div>
-</div>
-
+ <!--footer start-->
+ @include('include.footer')
+  <!--footer end-->
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
