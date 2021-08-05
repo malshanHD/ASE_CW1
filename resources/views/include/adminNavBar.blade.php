8e62c015eb373f346b582333b686accd285e3a58
mithun-1994 <mithunyasangika456@gmail.com>
2021-08-05 17:36:47 +0530

    change admin Nav-bar

--- resources/views/include/adminNavBar.blade.php
+++ resources/views/include/adminNavBar.blade.php
@@ -1,3 +1,4 @@
+<!--start Admin Nav bar-->
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="background: linear-gradient(90deg, rgba(233,255,4,1) 0%, rgba(1,21,81,1) 0%, rgba(1,21,81,1) 30%, rgba(1,21,81,1) 61%, rgba(1,21,81,1) 100%);">
 <a class="navbar-brand" href="#"><img src="SystemImage/logov1.png" style="width: 200px;" alt=""></a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,23 +7,24 @@
   <div class="collapse navbar-collapse justify-content-end text-uppercase font-weight-bold" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item ">
-        <a class="nav-link" href="#">Slide Change</a>
+        <a class="nav-link active" href="#">Slide Change</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="#"><u>Buyer Manage</u></a>
+        <a class="nav-link" href="#">Buyer Manage</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="#">Seller Manage</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="#">Dashboard</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="#">Account</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item active">
         <a class="nav-link" href="#">Logout</a>
       </li>
     </ul>
   </div>
 </nav>
+<!--End Admin Nav bar-->
