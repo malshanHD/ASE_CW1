bc997d924122a31ad749a46757a4457cbc95761a
mithun-1994 <mithunyasangika456@gmail.com>
2021-08-05 12:11:12 +0530

    Create seller Navbar

---
+++ resources/views/include/sellerNavbar.blade.php
@@ -0,0 +1,29 @@
+<!-- start navbar start-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="background: linear-gradient(90deg, rgba(233,255,4,1) 0%, rgba(1,21,81,1) 0%, rgba(1,21,81,1) 30%, rgba(1,21,81,1) 61%, rgba(1,21,81,1) 100%);">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse justify-content-center text-uppercase font-weight-bold" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Sales Dashboard</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">New Item </a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Check Orders</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Check Feedbacks</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Messages</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Logout</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!-- nav-bar end-->
