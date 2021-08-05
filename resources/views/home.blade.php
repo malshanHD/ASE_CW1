be4388d95e3990a61533e0073da841a14be6acc4
Dhanushka ChanakaMadhusanka <dhanushkaak47@gmail.com>
2021-08-05 12:00:29 +0530

    add nav bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,34 +18,7 @@
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="background: linear-gradient(90deg, rgba(233,255,4,1) 0%, rgba(1,21,81,1) 0%, rgba(1,21,81,1) 30%, rgba(1,21,81,1) 61%, rgba(1,21,81,1) 100%);">
-<a class="navbar-brand" href="#"><img src="SystemImage/logov1.png" style="width: 200px;" alt=""></a>
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse justify-content-end text-uppercase font-weight-bold" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item ">
-        <a class="nav-link" href="#">Language</a>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#"><u>Item Buy</u></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Wish List</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Help</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Account</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Logout</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+@include('include.BuyerNavBar')
 
 <form class="md-form form-sm mt-0 text-center" action="" method="get">
             {{csrf_field()}}
