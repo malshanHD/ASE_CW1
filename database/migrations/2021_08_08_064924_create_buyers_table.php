46228ec815e29fd0a20ce569810c859a01ff3d3b
Dhanushka ChanakaMadhusanka <dhanushkaak47@gmail.com>
2021-08-08 12:48:01 +0530

    crate buyer DB table

---
+++ database/migrations/2021_08_08_064924_create_buyers_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBuyersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buyers', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('gender');
+            $table->date('dob');
+            $table->string('email');
+            $table->integer('phnNumber');
+            $table->string('country');
+            $table->string('streetadd01');
+            $table->string('streetadd02');
+            $table->string('city');
+            $table->string('province');
+            $table->integer('zipcode');
+            $table->string('image');
+            $table->string('username');
+            $table->string('password');
+            $table->string('confirmpassword');
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buyers');
+    }
+}
