fe2f10ff85486653364157ea60c391d39fa49a70
milanbc <milanjayasinghe1234@gmail.com>
2021-08-10 12:11:44 +0530

    Update database coding og homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -209,7 +209,7 @@
 <div class="container mt-3" style="background: rgb(241,241,241);">
     <div class="row">
           <div class="col-12 col-md-3 mt-2">
-            <h5 class="font-weight-bold"><i class="fa fa-cog" aria-hidden="true"></i> Electric Items</h5>
+            <h5 class="font-weight-bold"><i class="fa fa-book" aria-hidden="true"></i> Books</h5>
             <hr>
           </div>
     </div>
@@ -230,9 +230,9 @@
     </div>
 </div>
 <div class="container mt-3" style="background: rgb(241,241,241);">
-    <div class="row">
+    <div class="row ">
           <div class="col-12 col-md-3 mt-2">
-            <h5 class="font-weight-bold"><i class="fa fa-book" aria-hidden="true"></i> Books</h5>
+            <h5 class="font-weight-bold"><i class="fa fa-mobile" aria-hidden="true"></i> Electronic Items</h5>
             <hr>
           </div>
     </div>
@@ -250,27 +250,6 @@
             </div>
           </div> 
       @endforeach 
-    </div>
-</div>
-<div class="container mt-3" style="background: rgb(241,241,241);">
-    <div class="row ">
-          <div class="col-12 col-md-3 mt-2">
-            <h5 class="font-weight-bold"><i class="fa fa-mobile" aria-hidden="true"></i> Electronic Items</h5>
-            <hr>
-          </div>
-    </div>
-    <div class="row">
-      <div class="col-12 col-md-3 mb-2">
-            <div class="card" style="width: 100%;">
-               <div class="card-body">
-                    <img src="AddItemsImages/eleI1.JPG" style="width: 100%;" height="auto" alt=""> 
-                    <p><b>Item name</b></p>
-                   <span>
-                     <p><s>$66</s> &nbsp &nbsp $55</p>
-                   </span>            
-                </div>
-              </div>
-            </div>  
       </div>
 </div>
     
