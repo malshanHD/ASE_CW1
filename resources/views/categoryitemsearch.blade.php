b8cf325691f385c663877c7f862e4c95125c5d03
milanbc <milanjayasinghe1234@gmail.com>
2021-08-10 15:22:33 +0530

    Design of the Category Item search page

--- resources/views/categoryitemsearch.blade.php
+++ resources/views/categoryitemsearch.blade.php
@@ -4,9 +4,88 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Category Item Search</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+  <style>
+    hr {
+      border-top: 2px solid orange;
+    }
+    
+  </style>
 </head>
 <body>
-    
+@include('include.BuyerNavBar') 
+<div class="container">
+    <div class="row mt-4">
+        <div class="col">
+        <p class="text-uppercase font-weight-bold text-warning"><span><a class="text-info" href="/">Home</a></span>><span>Category Name</span></p>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row mt-3">
+        <div class="col">
+        <h5 class="font-weight-bold">Category Items</h5>
+        </div>
+    </div>
+</div>
+<div class="container mt-5" style="background: rgb(241,241,241);">
+    <div class="row">
+          <div class="col-12 col-md-3 mb-2">
+            <div class="card" style="width: 100%;">
+              <div class="card-body">
+                <img class="card-img-top" src="" style="width:100%; height:100%;" alt="Card image cap"> 
+                   <p><b><a href=""></a></b></p>
+                   <span>
+                      <p></p>
+                   </span>         
+              </div>
+            </div>
+          </div> 
+          <div class="col-12 col-md-3 mb-2">
+            <div class="card" style="width: 100%;">
+              <div class="card-body">
+                <img class="card-img-top" src="" style="width:100%; height:100%;" alt="Card image cap"> 
+                   <p><b><a href=""></a></b></p>
+                   <span>
+                      <p></p>
+                   </span>         
+              </div>
+            </div>
+          </div> 
+          <div class="col-12 col-md-3 mb-2">
+            <div class="card" style="width: 100%;">
+              <div class="card-body">
+                <img class="card-img-top" src="" style="width:100%; height:100%;" alt="Card image cap"> 
+                   <p><b><a href=""></a></b></p>
+                   <span>
+                      <p></p>
+                   </span>         
+              </div>
+            </div>
+          </div> 
+          <div class="col-12 col-md-3 mb-2">
+            <div class="card" style="width: 100%;">
+              <div class="card-body">
+                <img class="card-img-top" src="" style="width:100%; height:100%;" alt="Card image cap"> 
+                   <p><b><a href=""></a></b></p>
+                   <span>
+                      <p></p>
+                   </span>         
+              </div>
+            </div>
+          </div> 
+    </div>
+</div>
+
+@include('include.footer')
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 </body>
 </html>
